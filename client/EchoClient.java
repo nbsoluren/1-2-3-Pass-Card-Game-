e319e2a8a64bd3394ed07e42a17407d4d72f590d
nbsoluren <nbsoluren@up.edu.ph>
2019-11-06 06:00:08 +0800

    Fixed transmission issue

--- client/EchoClient.java
+++ client/EchoClient.java
@@ -1,54 +1,52 @@
 package client;
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
+
 import gameUi.Card;
-public class EchoClient{
-  public static void main(String[] args){
 
-  try{
-    String serverName = "localhost";
-    int port = 8818;
+public class EchoClient {
+    public static void main(String[] args) {
 
-    System.out.println("Client started");
+        try {
+            String serverName = "localhost";
+            int port = 8818;
+
+            System.out.println("Client started");
 //    Socket soc = new Socket("localhost", 8818);
-    System.out.println("Connecting to " + serverName + " on port " + port);
-    Socket client = new Socket(serverName, port);
-    /* Send data to the ServerSocket */
-    System.out.println("Just connected to " + client.getRemoteSocketAddress());
+            System.out.println("Connecting to " + serverName + " on port " + port);
+            Socket client = new Socket(serverName, port);
+            /* Send data to the ServerSocket */
+            System.out.println("Just connected to " + client.getRemoteSocketAddress());
 
-    PrintWriter out = new PrintWriter(client.getOutputStream(), true); //true autoflush data, immediately send import junit.framework.TestCase;
-    out.println("received");
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true); //true autoflush data, immediately send import junit.framework.TestCase;
+            out.println("received");
 //    Thread.sleep(10000);
 
-    BufferedReader in  = new BufferedReader(new InputStreamReader(client.getInputStream()));
-    String cardString = in.readLine();
-    System.out.println("Cards from server:" + cardString + ". Enter card to pass.");
-    new Card(cardString);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String cardString = in.readLine();
+            System.out.println("Cards from server:" + cardString + ". Enter card to pass.");
+            new Card(cardString);
 
 
+            while (true) {
 
-    while (true){
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Enter a string");
+                String str = userInput.readLine();
 
-//    BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-//    System.out.println("Enter a string");
-//    String str = userInput.readLine();
-//
-//    //sending to the server
-//    PrintWriter out = new PrintWriter(soc.getOutputStream(), true); //true autoflush data, immediately send import junit.framework.TestCase;
-//    out.println("received");
-//
-//
-//    BufferedReader in  = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-//    System.out.println(in.readLine());
+                //sending to the server
+                out.println("received");
+
+                System.out.println(in.readLine());
 
 
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-
-
-  }catch(Exception e){
-    e.printStackTrace();
-  }
-  }
 }
